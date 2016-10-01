130f13c4226db6f52e2b91c2f6a2ba8c4aa3e87b
Etienne Fichot <fichotetienne@gmail.com>
2016-10-01 18:01:56 +0200

    add some fct

--- libft.h
+++ libft.h
@@ -5,8 +5,13 @@
 # include <stdlib.h>
 # include <string.h>
 
-void ft_putchar(char c);
-void ft_putstr(char *str);
-void ft_putnbr(int nb);
+void  ft_putchar(char c);
+void  ft_putstr(char *str);
+void  ft_putnbr(int nb);
+int   ft_atoi(char *str);
+void  *ft_memset(void *b, int c, size_t len);
+void  ft_bzero(void *s, size_t n);
+void  *ft_memcpy(void *dst, const void *src, size_t n);
+void  *ft_memccpy(void *dest, const void *src, int c, size_t n);
 
 #endif
--- main.c
+++ main.c
@@ -2,11 +2,21 @@
 
 int   main()
 {
+  char buffer[] = "coucou c'est encore moi!";
+
   ft_putchar('a');
   ft_putchar('\n');
   ft_putstr("coucou");
   ft_putchar('\n');
   ft_putnbr(3);
+  ft_putchar('\n');
+  ft_putnbr(ft_atoi("-42"));
+  ft_putchar('\n');
+  ft_putstr((char *)ft_memset(buffer, 97, 6));
+  ft_putchar('\n');
+  ft_putstr((char *)ft_memcpy(buffer, "helloo", 6));
+  ft_putchar('\n');
+  ft_putstr((char *)ft_memccpy(buffer, "helloo", 101, 6));
   ft_putchar('\n');
   return (0);
 }
